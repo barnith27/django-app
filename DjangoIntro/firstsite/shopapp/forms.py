7af93e20d03c328c80782d7c5a8dfeaf025b9370
Кристина Брусник <kristya.27@mail.ru>
2025-08-12 19:58:05 +0500

    homework #8

--- DjangoIntro/firstsite/shopapp/forms.py
+++ DjangoIntro/firstsite/shopapp/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Product, Order
+from django.contrib.auth.models import Group
+
 
 class ProductForm(forms.ModelForm):
     class Meta:
@@ -10,3 +12,8 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = 'delivery_address', 'promocode', 'user', 'products'
+
+class GroupForm(forms.ModelForm):
+    class Meta:
+        model = Group
+        fields = ['name']
--- DjangoIntro/firstsite/shopapp/views.py
+++ DjangoIntro/firstsite/shopapp/views.py
@@ -1,71 +1,106 @@
+from operator import truediv
 from timeit import default_timer
 
 from django.contrib.auth.models import Group
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, redirect, reverse
+from django.contrib.messages import success
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from dns.update import Update
 
-from .forms import ProductForm, OrderForm
+from .forms import ProductForm, OrderForm, GroupForm
 from .models import Product
 from .models import Order
 
-def shop_index(request: HttpRequest):
-    products = [
-        ('laptop', 1000),
-        ('desktop', 1600),
-        ('smartphone', 600),
-    ]
-    context = {
-        "time_running": default_timer(),
-        "products": products
-    }
-    return render(request, 'shopapp/shop-index.html', context=context)
+class ShopIndexView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        products = [
+            ('laptop', 1000),
+            ('desktop', 1600),
+            ('smartphone', 600),
+        ]
+        context = {
+            "time_running": default_timer(),
+            "products": products
+        }
+        return render(request, 'shopapp/shop-index.html', context=context)
 
 
-def groups_list(request: HttpRequest):
-    context = {
-        'groups': Group.objects.prefetch_related('permissions').all(),
-    }
-    return render(request, 'shopapp/groups-list.html', context=context)
+class GroupsListView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        context = {
+            'form': GroupForm(),
+            'groups': Group.objects.prefetch_related('permissions').all(),
+        }
+        return render(request, 'shopapp/groups-list.html', context=context)
 
-def products_list(request: HttpRequest):
-    context = {
-        'products': Product.objects.all(),
-    }
-    return render(request, 'shopapp/products-list.html', context=context)
-
-def create_product(request: HttpRequest) -> HttpResponse:
-    if request.method == 'POST':
-        form = ProductForm(request.POST)
+    def post(self, request: HttpRequest):
+        form = GroupForm(request.POST)
         if form.is_valid():
             form.save()
-            url = reverse('shopapp:products_list')
-            return redirect(url)
-    else:
-        form = ProductForm()
-    context = {
-        'form': form,
-    }
+        return redirect(request.path)
 
-    return render(request, 'shopapp/create-product.html', context=context)
+class ProductDetailsView(DetailView):
+    template_name = 'shopapp/products-details.html'
+    model = Product
+    context_object_name = 'product'
 
+class ProductsListView(ListView):
+    template_name = 'shopapp/products-list.html'
+    model = Product
+    context_object_name = 'products'
+    queryset = Product.objects.filter(archived=False)
 
-def orders_list(request: HttpRequest):
-    context = {
-        'orders': Order.objects.select_related("user").prefetch_related("products").all(),
-    }
-    return render(request, 'shopapp/order-list.html', context=context)
+class ProductCreateView(CreateView):
+    model = Product
+    fields = 'name', 'price', 'description', 'discount'
+    success_url = reverse_lazy('shopapp:products_list')
 
-def create_order(request: HttpRequest) -> HttpResponse:
-    if request.method == 'POST':
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
-            url = reverse('shopapp:products_list')
-            return redirect(url)
-    else:
-        form = OrderForm()
-    context = {
-        'form': form,
-    }
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = 'name', 'price', 'description', 'discount'
+    template_name_suffix = '_update_form'
 
-    return render(request, 'shopapp/create-order.html', context=context)
+    def get_success_url(self):
+        return reverse('shopapp:product_details', kwargs={'pk': self.object.pk})
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('shopapp:products_list')
+
+    def form_valid(self, form):
+        success_url = self.get_success_url()
+        self.object.archived = True
+        self.object.save()
+        return HttpResponseRedirect(success_url)
+
+class OrdersListView(ListView):
+    queryset = (
+        Order.objects
+        .select_related("user")
+        .prefetch_related("products"))
+
+class OrderDetailView(DetailView):
+    queryset = (
+        Order.objects
+        .select_related("user")
+        .prefetch_related("products"))
+
+class OrderCreateView(CreateView):
+    model = Order
+    fields = 'delivery_address', 'promocode', 'user', 'products'
+    success_url = reverse_lazy('shopapp:orders_list')
+
+class OrderUpdateView(UpdateView):
+    model = Order
+    fields = 'delivery_address', 'promocode', 'user', 'products'
+    template_name_suffix = '_update_form'
+
+    def get_success_url(self):
+        return reverse('shopapp:order_details', kwargs={'pk': self.object.pk})
+
+class OrderDeleteView(DeleteView):
+    model = Order
+    success_url = reverse_lazy('shopapp:orders_list')
